f53a173164ce5f4d4a7f04ffc392b3af47a97b19
Leah Ajmani <lha37@cornell.edu>
2020-05-21 09:29:37 -0400

    added plot descriptions

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,8 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  document.body.style.zoom="75%"
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
